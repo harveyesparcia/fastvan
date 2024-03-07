76329da4ab47aa14f070cc26980538a2255695c3
LAPTOP-OEQMAT54\jon <stevebass777>
2024-03-07 22:32:24 +0800

    add bookings for passenger

--- capstone/Assets/PassengerView.cs
+++ capstone/Assets/PassengerView.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class PassengerView : MonoBehaviour
@@ -19,4 +22,35 @@ public class PassengerView : MonoBehaviour
         SceneManager.LoadScene("LoginScene");
         SceneManager.UnloadSceneAsync("Passenger");
     }
+
+    public void AddBookingsTapped()
+    {
+        StartCoroutine(Update_ScheduledTransactions());
+    }
+
+    IEnumerator Update_ScheduledTransactions()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("DriversId", Context.DriversId);
+
+        using UnityWebRequest request = UnityWebRequest.Post("http://www.aasimudin.cctc-ccs.net/Api/update_scheduledtransactions.php", form);
+
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            try
+            {
+                string jsonResponse = request.downloadHandler.text;
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+    }
 }
+
