bf2763c31f9586eaecfa135d6ff60aefd5613933
LAPTOP-F2AAA2PV\Harvey <harveyesparcia@gmail.com>
2024-03-24 23:18:19 +0800

    fix bug

--- capstone/Assets/LoginView.cs
+++ capstone/Assets/LoginView.cs
@@ -189,7 +189,7 @@ public class LoginView
         form.AddField("password", newpassword.text);
         form.AddField("firstname", firstname.text);
         form.AddField("lastname", lastname.text);
-        form.AddField("typeofaccount", 2);
+        form.AddField("typeofaccount", 3);
         form.AddField("BirthDate", date.text);
         form.AddField("Address", address.text);
         form.AddField("IsResign", 0);
